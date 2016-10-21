be26a7d3ea2b844a75e4a82a5decd974816b9be6
Paolo Savoldi <paooolino@gmail.com>
2016-10-21 17:46:01 +0200

    adding Header, Footer, Nav

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import Header from './Header';
+import Footer from './Footer';
+import Nav from './Nav';
+
 export default (props) => (
 	<div className="container">
 		{props.children}
+		<Nav />
+		<Header />
+		<Footer />
 	</div>
 );
